6cc9d0b7e6c2fcd56260042853488f4dceeb00ad
faisalmoeen <fmorakzai@gmail.com>
2018-01-05 21:53:24 +0100

    updated help message

--- src/main/java/input/MainApp.java
+++ src/main/java/input/MainApp.java
@@ -53,7 +53,7 @@ public class MainApp {
 			System.out.println("No commandline arguments found. Using default values instead");
 			System.out.println("Usage: .bin/spark-submit --class input.MainApp " +
 					"~/TrajectoryMining/TrajectoryMining-0.0.1-SNAPSHOT-jar-with-dependencies.jar " +
-					" e=15 p=10 h=87 s=174 r=1");
+					" e=15 p=10 h=87 s=174 r=1 master=local[4] input=/tmp/input.txt output=/tmp/output");
 			System.out.println("Missing values are replaced by defaults!");
 			System.exit(-1);
 		}
