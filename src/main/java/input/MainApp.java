a972e7c71ef47bf48fd3535d99217a961c45ee50
faisalmoeen <fmorakzai@gmail.com>
2018-01-05 21:51:17 +0100

    accept master url as param added input output paths as param

--- src/main/java/input/MainApp.java
+++ src/main/java/input/MainApp.java
@@ -20,52 +20,59 @@ import conf.AppProperties;
  *
  */
 public class MainApp {
-    
-    public static void main(String[] args) {
-	int hdfs_partitions = 87;
-	int eps = 15, minpt = 10 ;
-	int snapshot_partitions = 87;
-	int M = minpt;
-	int earth = 0;
-	if(args.length > 0) {
-	    for(String arg : args) {
-		System.out.println(arg);
-		if(arg.startsWith("h=")|| arg.startsWith("H=")) {
-		    hdfs_partitions = Integer.parseInt(arg.split("=")[1]);
-		} else if(arg.startsWith("e=")|| arg.startsWith("E=")) {
-		    eps = Integer.parseInt(arg.split("=")[1]);
-		} else if(arg.startsWith("p=")|| arg.startsWith("P=")) {
-		    minpt = Integer.parseInt(arg.split("=")[1]);
-		} else if(arg.startsWith("s=")|| arg.startsWith("S=")) {
-		    snapshot_partitions = Integer.parseInt(arg.split("=")[1]);
-		} else if(arg.startsWith("r=") || arg.startsWith("R=")) {
-		    earth = Integer.parseInt(arg.split("=")[1]);
+
+	public static void main(String[] args) {
+		int hdfs_partitions = 87;
+		int eps = 15, minpt = 10 ;
+		int snapshot_partitions = 87;
+		int M = minpt;
+		int earth = 0;
+		String master = null, input = null, output = null;
+		if(args.length > 0) {
+			for(String arg : args) {
+				System.out.println(arg);
+				if(arg.startsWith("h=")|| arg.startsWith("H=")) {
+					hdfs_partitions = Integer.parseInt(arg.split("=")[1]);
+				} else if(arg.startsWith("e=")|| arg.startsWith("E=")) {
+					eps = Integer.parseInt(arg.split("=")[1]);
+				} else if(arg.startsWith("p=")|| arg.startsWith("P=")) {
+					minpt = Integer.parseInt(arg.split("=")[1]);
+				} else if(arg.startsWith("s=")|| arg.startsWith("S=")) {
+					snapshot_partitions = Integer.parseInt(arg.split("=")[1]);
+				} else if(arg.startsWith("r=") || arg.startsWith("R=")) {
+					earth = Integer.parseInt(arg.split("=")[1]);
+				} else if(arg.startsWith("master=") || arg.startsWith("Master=")) {
+					master = arg.split("=")[1];
+				} else if(arg.startsWith("input=") || arg.startsWith("Input=")) {
+					input = arg.split("=")[1];
+				} else if(arg.startsWith("output=") || arg.startsWith("Output=")) {
+					output = arg.split("=")[1];
+				}
+			}
+		} else {
+			System.out.println("No commandline arguments found. Using default values instead");
+			System.out.println("Usage: .bin/spark-submit --class input.MainApp " +
+					"~/TrajectoryMining/TrajectoryMining-0.0.1-SNAPSHOT-jar-with-dependencies.jar " +
+					" e=15 p=10 h=87 s=174 r=1");
+			System.out.println("Missing values are replaced by defaults!");
+			System.exit(-1);
 		}
-	    }
-	} else {
-	    System.out.println("No commandline arguments found. Using default values instead");
-	    System.out.println("Usage: .bin/spark-submit --class input.MainApp " +
-	    		"~/TrajectoryMining/TrajectoryMining-0.0.1-SNAPSHOT-jar-with-dependencies.jar " +
-	    		" e=15 p=10 h=87 s=174 r=1");
-	    System.out.println("Missing values are replaced by defaults!");
-	    System.exit(-1);
+		String hdfs_input = input;
+		String hdfs_output = output;
+		String name = String.format("DBSCAN-E=%d-P=%d", eps, minpt);
+		Logger.getLogger("org").setLevel(Level.OFF);
+		Logger.getLogger("aka").setLevel(Level.OFF);
+
+
+		SparkConf conf = new SparkConf().setAppName(name).setMaster(master);
+		JavaSparkContext context = new JavaSparkContext(conf);
+		JavaRDD<String> inputRDD = context.textFile(hdfs_input, hdfs_partitions);
+		ClusteringMethod cm = new BasicClustering(eps, minpt, M, snapshot_partitions, earth);
+		JavaRDD<SnapshotClusters> CLUSTERS = cm.doClustering(inputRDD);
+		String hdfs_out = String.format(hdfs_output + "/clusters-e%d-p%d",
+				eps, minpt);
+		CLUSTERS.saveAsObjectFile(hdfs_out);
+		context.close();
 	}
-	String hdfs_input = AppProperties.getProperty("hdfs_input");
-	String hdfs_output = AppProperties.getProperty("hdfs_output");
-	String name = String.format("DBSCAN-E=%d-P=%d", eps, minpt);
-	Logger.getLogger("org").setLevel(Level.OFF);
-	Logger.getLogger("aka").setLevel(Level.OFF);
-	
-	
-	SparkConf conf = new SparkConf().setAppName(name);
-	JavaSparkContext context = new JavaSparkContext(conf);
-	JavaRDD<String> input = context.textFile(hdfs_input, hdfs_partitions);
-	ClusteringMethod cm = new BasicClustering(eps, minpt, M, snapshot_partitions, earth);
-	JavaRDD<SnapshotClusters> CLUSTERS = cm.doClustering(input);
-	String hdfs_out = String.format(hdfs_output + "/clusters-e%d-p%d",
-		eps, minpt);
-	CLUSTERS.saveAsObjectFile(hdfs_out);
-	context.close();
-    }
-    
+
 }
